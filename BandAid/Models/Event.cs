18389a22fd35529548a78b7e0cabcb06e0460d85
Tommy Škrak <tommyskrak1998@gmail.com>
2019-05-08 17:15:18 +0200

    Event Controller i Event model.

--- BandAid/Models/Event.cs
+++ BandAid/Models/Event.cs
@@ -1,22 +1,39 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BandAid.Models
 {
-    public partial class Event
-    {
+	public partial class Event
+	{
+		public int EventId { get; set; }
 
-        public int EventId { get; set; }
-        public string Name { get; set; }
-        public DateTime Date { get; set; }
-        public string Adress { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Description { get; set; }
-        public string ImgUrl { get; set; }
-        public int UserId { get; set; }
-        public int StatusId { get; set; }
+		[Required(ErrorMessage = "Ime događaja je obavezno")]
+		[Display(Name = "Događaj")]
+		public string Name { get; set; }
 
-        public Status Status { get; set; }
-        public User User { get; set; }
-    }
+		[Required(ErrorMessage = "Vrijeme događaja je obavezno")]
+		[DataType(DataType.DateTime, ErrorMessage = "Uneseno vrijeme nije točno")]
+		public DateTime Date { get; set; }
+
+		[Required(ErrorMessage = "Adresa je obavezna")]
+		[Display(Name = "Adresa")]
+		public string Adress { get; set; }
+
+		[Display(Name = "Kontakt broj")]
+		public string PhoneNumber { get; set; }
+
+		[Display(Name = "Opis")]
+		public string Description { get; set; }
+
+		[DataType(DataType.ImageUrl)]
+		public string ImgUrl { get; set; }
+		public int UserId { get; set; }
+		public int StatusId { get; set; }
+
+		public User User { get; set; }
+
+		public Status Status { get; set; }
+	}
 }
